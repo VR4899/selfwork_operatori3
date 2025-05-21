295f3844f1f26f091d6b0741034ee1821ff042f3
Vincenzo Rosa <vincenzo.rosa99@gmail.com>
2025-05-22 00:03:13 +0200

    aggiunto selfwork operatori 4

--- selfwork_operatori.js
+++ selfwork_operatori.js
@@ -31,3 +31,31 @@ console.log (`la somma dei due numeri è ${potenza}`);
 
 
 console.log(`la somma dei due numeri è ${somma} e la moltiplicazione è ${moltiplicazione} e la sottrazione è ${sottrazione} e la divisione è ${divisione} e la potenza è ${potenza}`);
+
+
+// selwork operatori 4
+
+let numero_gatti_totali = 50;
+console.log(`il numero totale di gatti è ${numero_gatti_totali}`);
+
+let numero_gatti_in_fila = 8;
+console.log(`il numero di gatti in fila è ${numero_gatti_in_fila}`);
+
+
+let numero_di_file = Math.floor(numero_gatti_totali / numero_gatti_in_fila);
+console.log (`il numero di file è ${numero_di_file}`);
+
+let numero_di_gatti_mancanti = numero_gatti_totali % numero_gatti_in_fila;
+console.log(`il numero di gatti mancanti per completatre la fila è ${numero_di_gatti_mancanti}`);
+
+let numero_gatti_mancati_nel_ultima_fila = numero_gatti_in_fila - numero_di_gatti_mancanti;
+console.log(`il numero di gatti mancanti per completare l'ultima fila e ${numero_gatti_mancati_nel_ultima_fila}`);
+
+console.log(`il numero totale di gatti è ${numero_gatti_totali} e il numero di gatti in fila è ${numero_gatti_in_fila} e il numero di file è ${numero_di_file} e il numero di gatti mancanti per completare la fila è ${numero_gatti_mancati_nel_ultima_fila} e il numero di gatti mancanti per completare l'ultima fila è ${numero_gatti_mancati_nel_ultima_fila}`);
+
+
+
+
+
+
+
